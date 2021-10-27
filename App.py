25913bf5726c40ed6a75a5d5fb2460679c6369b2
Samantha Lee <samantha.lee@article.com>
2021-10-27 16:49:25 -0700

    randomize given list of names

--- App.py
+++ App.py
@@ -2,6 +2,8 @@ import os
 # Use the package we installed
 from slack_bolt import App
 from dotenv import load_dotenv
+import json
+import random
 
 load_dotenv()
 
@@ -22,8 +24,20 @@ def sayHello(ack,respond,command, say):
 
 @app.command('/speak')
 def saySpeakingOrder(ack,command,say):
+    # get input string
+    convertToDict = json.dumps(command)
+    convertDictToString = json.loads(convertToDict)
+    inputString = convertDictToString["text"] # TODO: implement functionality where text is null
+
+    # convert string into names
+    names = inputString.split()
+    random.shuffle(names)
+
+    # convert list of names into string
+    outputString = ' '.join(names)
+
     ack()
-    say('my awesome speaking command')
+    say(outputString)
 
 # Start your app
 if __name__ == "__main__":
