4aed73af4cf82c1dd1bc7e04c2cd328a06027fb2
Naseem Hammoud <naseem.hammoud@article.com>
2021-10-28 13:49:31 -0700

    removed /wotd command, merged with /speak function, changed to /scrum command

--- App.py
+++ App.py
@@ -18,8 +18,8 @@ def sayHello(ack,say):
     ack()
     say('hello Capybara')
 
-@app.command('/speak')
-def saySpeakingOrder(ack,command,say):
+@app.command('/scrum')
+def GenerateScrumOrderAndWordOfTheDay(ack,command,say):
     ack()
     commandDict = json.dumps(command)
     commandStr = json.loads(commandDict)
@@ -36,17 +36,14 @@ def saySpeakingOrder(ack,command,say):
         for member in members:
             userInfo = app.client.users_info(user=member)
             name = userInfo["user"]["real_name"]
-            if name != 'SuperBot':
+            if name != 'Capybara':
                 names.append(name)
 
     random.shuffle(names)
     namesStr = '\n'.join(names)
-    say(namesStr)
+    say(scrapeWordOfTheDay() + '\n\n\n' + namesStr)
 
-@app.command('/wotd')
-def scrapeWotd(ack,say):
-    ack()
-
+def scrapeWordOfTheDay():
     url = 'https://www.dictionary.com/e/word-of-the-day/'
     try:
         r = requests.get(url)
@@ -63,7 +60,7 @@ def scrapeWotd(ack,say):
 
     wordOfTheDay = '*' + date + '*' + '\n' + word + '\n' + pronunciation + '\n' + definition
 
-    say(wordOfTheDay)
+    return wordOfTheDay
 
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
